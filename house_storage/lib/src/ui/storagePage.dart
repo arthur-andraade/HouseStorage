93bcbb70a2457ab71081cb56eaf440eb14ac2149
Arthur Vinicius Fogaça de Andrade <arthurandradenf@gmail.com>
2020-03-24 23:31:59 -0300

    Add alteração do nome de item

--- house_storage/lib/src/ui/storagePage.dart
+++ house_storage/lib/src/ui/storagePage.dart
@@ -15,18 +15,27 @@ class StoragePage extends StatefulWidget {
 //----------------> StoreState <----------------------
 
 class _StorageState extends State<StoragePage> {
-  //Variáveis
+  
+  // Variáveis
   String _rotulo;
   TextEditingController addNovoItem = new TextEditingController();
 
+  //  Controller que faz operações no DB
   Storage_help controllerDB = Storage_help();
+
+  //  Lista de itens do Storage
   List<Item> itemStorage = List();
+  
+  //  Controller que altera as "Quantidade" de cada item
   List<TextEditingController> _controllersQuantidade = List();
+  
+  //  Controller que altera as "Nome" de cada item
+  List<TextEditingController> _controllersNome = List();
 
   Item _itemRemovido;
   int _indexItemRemovido;
 
-  //Construtor da classe
+  //  Construtor da classe
   _StorageState(String rotulo) {
     this._rotulo = rotulo;
   }
@@ -36,22 +45,22 @@ class _StorageState extends State<StoragePage> {
     super.initState();
 
     controllerDB.setRotulo(_rotulo);
-    print(controllerDB.rotulo);
+    //print(controllerDB.rotulo);
     controllerDB.getTodosItens().then((list) {
       //list.forEach((f) {
       //print(f.id);
       //});
       setState(() {
         itemStorage = list;
-        addControllerQuantidadeParaItens();
+        addControllerQuantidadeENomeParaItens();
       });
     });
   }
 
-  //Construtor do "LAYOUT"
+  //  Construtor do "LAYOUT"
   @override
   Widget build(BuildContext context) {
-    //Layout
+    //  Layout
     return Scaffold(
       appBar: AppBar(
         title: Text('$_rotulo'),
@@ -60,12 +69,12 @@ class _StorageState extends State<StoragePage> {
       ),
       body: Column(
         children: <Widget>[
-          //ADICIONANDO NOVO ITEM
+          //  ADICIONANDO NOVO ITEM
           Container(
             padding: EdgeInsets.fromLTRB(17.0, 1.0, 7.0, 1.0),
             child: Row(
               children: <Widget>[
-                //Insere "NOME" do novo item
+                //  Insere "NOME" do novo item
                 Expanded(
                   child: TextField(
                     controller: addNovoItem,
@@ -75,7 +84,7 @@ class _StorageState extends State<StoragePage> {
                   ),
                 ),
 
-                //Botao para adicionar novo item a lista
+                //  Botao para adicionar novo item a lista
                 Container(
                   padding: EdgeInsets.all(5.0),
                   height: 70.0,
@@ -92,7 +101,7 @@ class _StorageState extends State<StoragePage> {
             ),
           ),
 
-          //Lista de itens
+          //  Lista de itens
           Expanded(
             child: ListView.builder(
               padding: EdgeInsets.only(top: 10.0, right: 12.0),
@@ -126,15 +135,31 @@ class _StorageState extends State<StoragePage> {
       child: Row(children: <Widget>[
         // Item "NOME"
         Expanded(
-          child: Text(
-            itemStorage[index].nome,
-            textAlign: TextAlign.center,
-            style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+          child: Padding(padding: EdgeInsets.only(left: 15.0, right: 5.0),
+            child: TextField(
+              controller: _controllersNome[index],
+              keyboardType: TextInputType.text,
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 25.0,
+                fontWeight: FontWeight.bold
+              ),
+
+              //  Atulizando nome alterado
+              onChanged: (nomeAlterado){
+                
+                _controllersNome[index].text = nomeAlterado;
+                itemStorage[index].nome = _controllersNome[index].text;
+                
+                //  Dando update no DB
+                var id = controllerDB.alterandoNomeDoIten(itemStorage[index]);
+              },
+            )
           )
         ),
 
-        //---->>> Quantidade <<--------------
-        //Adicionar +1 a quantidade do item
+        //  ---->>> Quantidade <<--------------
+        //  Adicionar +1 a quantidade do item
         IconButton(
           icon: Icon(
             Icons.add_circle,
@@ -147,12 +172,12 @@ class _StorageState extends State<StoragePage> {
             _controllersQuantidade[index].text = valorIncrementado.toString();
             itemStorage[index].quantidade = valorIncrementado;
 
-            //Dando upgrade no "DB"
+            //  Dando upgrade no "DB"
             var id = controllerDB.alterandoQuantidadeDeItem(itemStorage[index]);
           },
         ),
 
-        //Número de itens
+        //  Número de itens
         Container(
             padding: EdgeInsets.only(left: 2.5, right: 2.5),
             width: 50.0,
@@ -161,16 +186,21 @@ class _StorageState extends State<StoragePage> {
                 keyboardType: TextInputType.number,
                 textAlign: TextAlign.center,
                 controller: _controllersQuantidade[index],
+
+                //Altera número de itens
                 onChanged: (quantidadeAlterada) {
+                  
                   _controllersQuantidade[index].text = quantidadeAlterada;
                   itemStorage[index].quantidade =
                       int.parse(_controllersQuantidade[index].text);
+                  
                   //Dando upgrade no "DB"
                   var id = controllerDB
                       .alterandoQuantidadeDeItem(itemStorage[index]);
+
                 })),
 
-        //Diminuir +1 a quantidade do item
+        //  Diminuir +1 a quantidade do item
         IconButton(
           icon: Icon(
             Icons.remove_circle,
@@ -183,38 +213,43 @@ class _StorageState extends State<StoragePage> {
             _controllersQuantidade[index].text = valorIncrementado.toString();
             itemStorage[index].quantidade = valorIncrementado;
 
-            //Dando upgrade no "DB"
+            //  Dando upgrade no "DB"
             var id = controllerDB.alterandoQuantidadeDeItem(itemStorage[index]);
           },
         )
         //----------------------------------------------------------
       ]),
 
-      //Removendo item
+      //  Removendo item
       onDismissed: (direction) {
         _itemRemovido = itemStorage[index];
         _indexItemRemovido = index;
         itemStorage.removeAt(_indexItemRemovido);
         _controllersQuantidade.removeAt(_indexItemRemovido);
-
-        //Removendo do "DB"
+        _controllersNome.removeAt(_indexItemRemovido);
+        //  Removendo do "DB"
         var id = controllerDB.deletandoItem(_itemRemovido);
         print(id);
 
-        //Adicionando um SnackBar para desfazer o delete do item
+        //  Adicionando um SnackBar para desfazer o delete do item
         final snack = SnackBar(
           content: Text(" Item \"${_itemRemovido.nome}\" removido"),
           action: SnackBarAction(
               label: "Desfazer",
               onPressed: () {
                 setState(() {
-                  //Restaurando item e controller de quantidade
+                  //  Restaurando item 
                   itemStorage.insert(_indexItemRemovido, _itemRemovido);
-                  _controllersQuantidade.insert(
-                      _indexItemRemovido, new TextEditingController());
-                  _controllersQuantidade[_indexItemRemovido].text =
-                      itemStorage[_indexItemRemovido].quantidade.toString();
-                  //Salvando no "DB" novamente
+
+                  //  Restaurando controller de "Quantidade" do item
+                  _controllersQuantidade.insert(_indexItemRemovido, new TextEditingController());
+                  _controllersQuantidade[_indexItemRemovido].text = itemStorage[_indexItemRemovido].quantidade.toString();
+
+                  //  Restaurando controller de "Nome" do item
+                  _controllersNome.insert(_indexItemRemovido, new TextEditingController());
+                  _controllersNome[_indexItemRemovido].text = itemStorage[_indexItemRemovido].nome;
+                  
+                  //  Salvando no "DB" novamente
                   id = controllerDB.salvandoNovoItem(_itemRemovido);
                 });
               }),
@@ -228,7 +263,7 @@ class _StorageState extends State<StoragePage> {
   }
   //---------------------------------------------------------
 
-  //Função adicionando item no store
+  //  Função adicionando item no storage
   void addItem() async {
     Item itemNovo = new Item();
     itemNovo.storage = _rotulo;
@@ -238,22 +273,26 @@ class _StorageState extends State<StoragePage> {
     setState(() {
       itemStorage.add(itemNovo);
       _controllersQuantidade.add(new TextEditingController());
+      _controllersNome.add(new TextEditingController());
       _controllersQuantidade[_controllersQuantidade.length - 1].text =
           itemNovo.quantidade.toString();
+      _controllersNome[_controllersNome.length - 1].text = itemNovo.nome;
     });
 
     itemNovo.id = await controllerDB.salvandoNovoItem(itemNovo);
     //print(itemNovo.id);
   }
 
-  void addControllerQuantidadeParaItens() {
+  void addControllerQuantidadeENomeParaItens() {
     itemStorage.forEach((item) {
       _controllersQuantidade.add(new TextEditingController());
+      _controllersNome.add(new TextEditingController());
     });
 
     for (int tamanho = 0; tamanho < itemStorage.length; tamanho++) {
       _controllersQuantidade[tamanho].text =
           itemStorage[tamanho].quantidade.toString();
+      _controllersNome[tamanho].text = itemStorage[tamanho].nome;
     }
   }
 }
